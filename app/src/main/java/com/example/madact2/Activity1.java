43ea974eebc51ee7e52f1dd7a701984cb23eebc9
Joshua <joshuamiguel.dagatan.v@bulsu.edu.ph>
2022-03-28 04:37:22 -1100

    Act1

--- app/src/main/java/com/example/madact2/Activity1.java
+++ app/src/main/java/com/example/madact2/Activity1.java
@@ -9,6 +9,6 @@ public class Activity1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_1);
+        setContentView(R.layout.activity_6);
     }
 }
