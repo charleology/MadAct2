672281a41a111f6cafcc44d91aa34f8308f4c46a
charleology <cnbelano@gmail.com>
2022-03-30 02:52:57 +0800

    Takot sa Committment

--- app/src/main/java/com/example/madact2/MainActivity.java
+++ app/src/main/java/com/example/madact2/MainActivity.java
@@ -8,19 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, Activity2.class);
+                Intent i = new Intent(MainActivity.this, Activity4.class);
                 startActivity(i);
             }
         });
