4ef4e206b87ffff4cc9ceb2bb6ebfe79eff63f6a
Joshua <joshuamiguel.dagatan.v@bulsu.edu.ph>
2022-03-29 08:47:03 -1100

    Act1

--- app/src/main/java/com/example/madact2/MainActivity.java
+++ app/src/main/java/com/example/madact2/MainActivity.java
@@ -2,27 +2,37 @@ package com.example.madact2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
+    String names [] = {"Temperature Conversion","Currency Consversion", "Length Conversion"};
+    ListView lv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = (Button) findViewById(R.id.button);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        lv1=(ListView)findViewById(R.id.lv1);
+        ArrayAdapter<String> lvAdapter=new ArrayAdapter<String>(this, R.layout.mainact_list,R.id.tv3,names);
+        lv1.setAdapter(lvAdapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, Activity4.class);
-                startActivity(i);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0){
+                    //Act2
+                    startActivity(new Intent(MainActivity.this, Activity2.class));
+                }
             }
         });
 
